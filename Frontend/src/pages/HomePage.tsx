f1668264835a374991787359d8c5abb858cd5d8d
rakeshmuntha <rakeshmuntha27@gmail.com>
2025-08-15 18:00:46 +0530

    added mobile resposive

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
 
     return selectRightSidebar ? (
             // <div className='border w-full h-screen sm:px-[8%] sm:py-[3%]'>
-            <div className='border w-full h-screen px-[8%] py-[3%]'>
+            <div className='border w-full h-screen px-[6%] sm:py-[3%]'>
                 <div className={`backdrop-blur-xl bg-black border-[0.5px] border-[#2f2d2d] rounded-md overflow-hidden h-[100%] grid grid-cols-1 relative 
                 ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'} `}>
                 <Sidebar />
@@ -21,7 +21,7 @@ const HomePage = () => {
             </div>
         </div>)
         : (
-            <div className='border w-full h-screen px-[8%] py-[3%]'>
+            <div className='border w-full h-screen px-[6%] sm:py-[3%]'>
                 <div className={`backdrop-blur-xl bg-black border-[0.5px] border-[#2f2d2d] rounded-md overflow-hidden h-[100%] grid grid-cols-1 relative 
                 ${selectedUser ? 'md:grid-cols-[1fr_1.5fr] xl:grid-cols-[1fr_2fr]' : 'md:grid-cols-[1fr_1.4fr]'} `}>
                     <Sidebar />
